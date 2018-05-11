49742be8db022aad96149230159f019eedd3143f
at0x0ft <at0p0ft@outlook.com>
2018-05-11 15:25:22 +0900

    Round.java (Beta) completed (Round.javaの仮完成) .

--- src/Round.java
+++ src/Round.java
@@ -1,15 +1,15 @@
 import java.io.*;
-import java.net.*;
+import java.util.*;
 
 public class Round {
 
-    private List<Player> _playerQueue;   // ゲーム参加者の順番リスト
+    private LinkedList<Player> _playerQueue;   // ゲーム参加者の順番リスト
 
-    private List<Card> _deck;    // 山札
+    private LinkedList<Card> _deck;    // 山札
 
-    private List<Card> _discard; // 捨て札
+    private LinkedList<Card> _discard; // 捨て札
 
-    private List<Round> _loserList;  // 脱落したプレイヤーのリスト
+    private LinkedList<Round> _loserList;  // 脱落したプレイヤーのリスト
 
     private boolean _hasDuchess; // 女公爵が山札に含まれているか否か
 
@@ -17,38 +17,138 @@ public class Round {
 
     private boolean _hasKing;    // 王が山札に含まれているか否か
 
-    public Round(List<Player> playerQueue, List<Card> deck, boolean hasDuchess, boolean hasPrince, boolean hasKing) {
+    public Round(LinkedList<Player> playerQueue, LinkedList<Card> deck, boolean hasDuchess, boolean hasPrince, boolean hasKing) {
         // 初期化処理
-        this.playerQueue 
+        this._playerQueue = playerQueue;
+        this._deck = deck;
+        this._discard = new LinkedList<Card>();
+        this._loserList = new LinkedList<Round>();
+        this._hasDuchess = hasDuchess;
+        this._hasPrince = hasPrince;
+        this._hasKing = hasKing;
     }
 
-    public static final int PORT = 8080; // Set the Port Number.
-
-    public static void main(String[] args) throws IOException {
-        ServerSocket s = new ServerSocket(PORT); // Construct the socket with port number.
-        System.out.println("Started: " + s);
-        try {
-            Socket socket = s.accept(); // Wait the connection setting requirements.
-            try {
-                System.out.println("Connection accepted: " + socket);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Set the buffer for data serving.
-                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
-                        true); // Set the buffer for sending.
-
-                while (true) {
-                    String str = in.readLine(); // Serving data.
-                    if (str.equals("END"))
-                        break;
-
-                    System.out.println("Echoing : ");
-                    out.println(str); // Sending data.
-                }
-            } finally {
-                System.out.println("closing...");
-                socket.close();
+    // LoveLetterのゲームの1ラウンドを開始するメソッド.
+    public LinkedList<Player> start() {
+        while(true) {
+            turn();
+            if(finished()) {
+                winJudge();
+                break;
             }
-        } finally {
-            s.close();
+        }
+    }
+
+    // 一人のプレイヤーがカードを引き、その効果を発揮させるメソッド
+    private void turn() {
+        Player turnPlayer = this._playerQueue.pop();
+        turnPlayer.resetProtection();
+
+        Card drawCard = this._deck.pop();
+        if(checkKing(drawCard)) {
+            lose(turnPlayer);
+            throwCard(drawCard);
+            return;
+        }
+        else if(checkDuchessMinister(drawCard)) {
+            if(turnPlayer.getHand().getStrength() >= 5) {
+                if(drawCard == "Minister") {
+                    lose(turnPlayer);
+                    throwCard(drawCard);
+                    return;
+                }
+                else if(drawCard == "Duchess") {
+                    turnAround(turnPlayer);
+                    throwCard(drawCard);
+                    return;
+                }
+
+                Card invokeCard = selectCard(turnPlayer, drawCard);
+                throwCard(invokeCard);
+
+                if(checkPrincessPrince(invokeCard)) {
+                    lose(turnPlayer);
+                    return;
+                }
+                else if(checkMonk(invokeCard)) {
+                    turnPlayer.setProtection();
+                    turnAround(turnPlayer);
+                    return;
+                }
+
+                execute(turnPlayer, invokeCard);
+                return;
+            }
+        }
+    }
+
+    // playerをplayerQueueの末尾に追加するメソッド
+    private void turnAround(Player player) {
+        _playerQueue(player);
+    }
+
+    // cardを捨て札の一番上に追加するメソッド
+    private void throwCard(Card card) {
+        this._discard.add(0, card);
+    }
+
+    // 負けプレイヤーの敗戦処理を行うメソッド
+    private void lose(Player loser) {
+        throwCard(loser.getHand());
+        loser.setHand(null);
+        loser.resetProtection();
+        this._loserList.add(loser);
+    }
+
+    // 山札から引いたカードを手札に残すか否かを確認するメソッド (詳細は後ほど)
+    private Card selectCard(Player selectPlayer, Card drawCard) {
+        // 選択を促すウィンドウの表示
+        // return drawCard;
+        // return selectPlayer.exchange(draw);
+    }
+
+    // cardが王であるかどうかをチェックするメソッド
+    private boolean checkKing(Card card) {
+        return this._hasKing && card.getName() == "King";
+    }
+
+    // cardが女公爵や大臣であるかどうかをチェックするメソッド
+    private boolean checkDuchessMinister(Card card) {
+        return (this._hasDuchess && card.getName() == "Duchess") || card.getName() == "Minister";
+    }
+
+    // cardが僧侶であるかどうかをチェックするメソッド
+    private boolean checkMonk(Card card) {
+        return card.getName() == "Monk";
+    }
+
+    // cardが姫や王子であるかをチェックするメソッド
+    private boolean checkPrincessPrince(Card card) {
+        return card.getName() == "Princess" || (this._hasPrince && card.getName() == "Prince");
+    }
+
+    // invokeCardに書かれた効果 (兵士、道化、騎士、魔術師、将軍の効果のみ) を実行するメソッド
+    private void execute(Card invokeCard) {
+        // 後で考える.
+    }
+
+    // ゲームが終了したか否かを判定するメソッド
+    private boolean finished() {
+        return _playerQueue.size() == 1 || _deck.size() == 1;
+    }
+昇順
+    // 勝者を決定し、勝ち点を与えるメソッド
+    private void winJudge() {
+        if(_playerQueue.size() != 1) {
+            sort(_playerQueue, /* 降順にソート */);
+            // 勝者の数が複数人なら引き分け
+
+            
+        }
+        
+        Player winPlayer = this._playerQueue[0];
+        if(winPlayer.getHand().getStrength() <= 7) {
+            // 面倒なので後で実装.
         }
     }
 }
