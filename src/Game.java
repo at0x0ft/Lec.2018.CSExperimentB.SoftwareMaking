2d8b1165b58cabcf0eee770de8efc79a2188c8c6
永井康博 <yasuhironagai@yasuhiro-nagai-Air.local>
2018-05-11 14:51:10 +0900

    Gameclass(修正2回目)

--- src/Game.java
+++ src/Game.java
@@ -1,36 +1,34 @@
 public class Game{
   public static void main(String[] args){
 
-    private List <Player> _playerList;
+    private LinkedList <Player> _playerList; //カードの参加者リスト
 
-    private List <Card> _cardList;
+    private LinkedList <Card> _cardList;//使うカードのリスト
 
-    private List <Round> _finishedRoundList;
+    private LinkedList <Round> _finishedRoundList;//ラウンドのリスト
 
-    private boolean _finished;
+    private boolean _finished;//ゲームが終了したか否か
 
-    private boolean _hasDuchess;
+    private boolean _hasDuchess;//女公爵が山札に含まれているか否か
 
-    private boolean _hasPrince;
+    private boolean _hasPrince;//王子が山札に含まれているか否か
 
-    private boolean _hasKing;
+    private boolean _hasKing;//王が山札に含まれているか否か
 
     public Game(boolean hasDuchess. boolean hasPrince. boolean hasKing);
 
-    public static int getPlayerNumber ();
+    public static int getPlayerNumber ();//現在のゲームの参加者人数を返すメソッド
 
-    public static int getNowRound ();
+    public static int getNowRound ();//現在のラウンドを返すメソッド
 
-    private hasFinished ();
+    private hasFinished ();//このゲームが終了したかどうか判定するメソッド
 
-    private String registerName ();
+    private String registerName ();//セッションしたプレイヤーに名前の入力を促すメソッド
 
-    private String registerName ();
+    private player createPlayer (String playerName);//名前の入力を求める
 
-    private player createPlayer (String playerName);
+    private Round startGame ();//ゲームを開始し終了したラウンドクラスを返す
 
-    private Round startGame ();
-
-    private boolean viewLogFile ();
+    private boolean viewLogFile ();//ゲーム終了時にゲームログファイルを出力する
   }
 }
