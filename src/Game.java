d6b1f0899d79559902bf9e342eb65c5223180213
永井康博 <yasuhironagai@yasuhiro-nagai-Air.local>
2018-05-11 14:22:15 +0900

    Gameclass(ゲームクラスを作成しました)

---
+++ src/Game.java
@@ -0,0 +1,36 @@
+public class Game{
+  public static void main(String[] args){
+
+    private List <Player> playerList
+
+    private List <Card> cardList
+
+    private List <Round> finishedRoundList
+
+    private boolean finished
+
+    private boolean hasDuchess
+
+    private boolean hasPrince
+
+    private boolean hasKing
+
+    public Gameclass(boolean hasDuchess. boolean hasPrince. boolean hasKing)
+
+    public static int getPlayerNumber ()
+
+    public static int getNowRound ()
+
+    private hasFinished ()
+
+    private String registerName ()
+
+    private String registerName ()
+
+    private player createPlayer (String playerName)
+
+    private Round startGame ()
+
+    private boolean viewLogFile ()
+  }
+}
