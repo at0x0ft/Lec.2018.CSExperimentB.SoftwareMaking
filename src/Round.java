91ac1b152c12f8251fa9a9aafe77d850c91e4663
at0x0ft <at0p0ft@outlook.com>
2018-05-11 13:09:09 +0900

    Created Round.java (Round.javaファイルの作成).

---
+++ src/Round.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class Round {
+    public static final int PORT = 8080; // Set the Port Number.
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket s = new ServerSocket(PORT); // Construct the socket with port number.
+        System.out.println("Started: " + s);
+        try {
+            Socket socket = s.accept(); // Wait the connection setting requirements.
+            try {
+                System.out.println("Connection accepted: " + socket);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Set the buffer for data serving.
+                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+                        true); // Set the buffer for sending.
+
+                while (true) {
+                    String str = in.readLine(); // Serving data.
+                    if (str.equals("END"))
+                        break;
+
+                    System.out.println("Echoing : ");
+                    out.println(str); // Sending data.
+                }
+            } finally {
+                System.out.println("closing...");
+                socket.close();
+            }
+        } finally {
+            s.close();
+        }
+    }
+}
